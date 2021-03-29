976500beab902ab65425f47ee8644f5534f2ae6f
lucas <lucas.singh10@gmail.com>
2021-03-29 00:04:48 -0300

    first commit

---
+++ js/algoritmos.js
@@ -0,0 +1,26 @@
+
+
+const primeroEnAnchura = () => {
+
+    console.log('Ha elegido el algoritmo primero en anchura');
+}
+
+const primeroEnProfundidad = () => {
+
+    console.log('Ha elegido el algoritmo primero en profundidad');
+}
+
+const maximaPendiente = () => {
+
+    console.log('Ha elegido el algoritmo Escalada con maxima pendiente');
+}
+
+const primeroElMejor = () => {
+
+    console.log('Ha elegido el algoritmo primero el mejor');
+}
+
+const algoritmoA = () => {
+
+    console.log('Ha elegido el algoritmo A*');
+}
